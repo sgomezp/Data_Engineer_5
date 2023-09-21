c663c5a5340d40162740d6cc02e6f88a83f0d300
Camilo Andres Vanegas <kamilo_vanegas@hotmail.com>
2023-09-21 09:16:45 +0200

    fix(app): Change params to connect databases
    Add env param, and upload example env

--- datagen/kafka_push.py
+++ datagen/kafka_push.py
@@ -1,10 +1,16 @@
 from kafka import KafkaProducer, KafkaConsumer
+from pymongo import MongoClient
 from data_generator import RandomDataGenerator
 import json
 
 def make_producer(host):
     producer = KafkaProducer(bootstrap_servers=[host], value_serializer=lambda x: json.dumps(x).encode("utf-8"))
     return producer
+
+def insert_mongo(document):
+    client = MongoClient("mongodb://localhost:27018")
+    database = client["mongo_datalake"]
+    database["Users"].insert_one(document)
 
 def send_random(producer):
     for msg in RandomDataGenerator():
@@ -14,6 +20,14 @@ def send_random(producer):
 def main():
     producer = make_producer(host="kafka:9092")
     send_random(producer)
+    data = {
+        "FirstName": "Alejandro",
+        "LastName": "Gonzalez",
+        "Age": 40,
+        "City": "Santiago",
+    }
+    insert_mongo(data)
+    print("Documento insertado")
 
 if __name__ == "__main__":
     main()
